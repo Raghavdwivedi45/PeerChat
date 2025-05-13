803232090b173fef67036e7d8be03a0ec5516a61
Raghav Dwivedi <rjkb143@gmail.com>
2025-05-14 01:40:37 +0530

    ADDED react_router_dom TO deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { fileURLToPath } from 'url';
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, io, server } from "./lib/socket.js";
 import path from "path";
+
 dotenv.config();
 
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
@@ -25,11 +26,13 @@ app.use("/api/messages", messageRoutes);
 
 
 if(process.env.NODE_ENV==="production") {
-    const frontendPath = path.join(__dirname, "../frontend/vite-project/dist");
-    app.use(express.static(frontendPath));
-    
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const viteBuildPath = path.join(__dirname, '../frontend/vite-project/dist');
+    app.use(express.static(viteBuildPath));
+
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(viteBuildPath, 'index.html'));
     });
 }
 
@@ -38,4 +41,5 @@ server.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`)
     connectDB();
 });
+
 
