f2f57e3577609da7f6376aa8f01f8ad70276eed6
Raghav Dwivedi <rjkb143@gmail.com>
2025-05-14 00:26:11 +0530

    Done all

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,10 +20,10 @@ app.use(cors({
 }))
 
 if(process.env.NODE_ENV==="production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/vite-project/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "/vite-project", "/dist", "/index.html"));
     })
 }
 
