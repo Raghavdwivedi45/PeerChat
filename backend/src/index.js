e3e35627707716995698290247ed2c09dc093ac2
Raghav Dwivedi <rjkb143@gmail.com>
2025-05-14 01:06:29 +0530

    ADDED react_router_dom TO deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,17 +19,20 @@ app.use(cors({
     credentials: true
 }))
 
-if(process.env.NODE_ENV==="production") {
-const frontendPath = path.join(__dirname, "../frontend/vite-project/dist");
-app.use(express.static(frontendPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
-}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+
+if(process.env.NODE_ENV==="production") {
+    const frontendPath = path.join(__dirname, "../frontend/vite-project/dist");
+    app.use(express.static(frontendPath));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
+
 
 server.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`)
