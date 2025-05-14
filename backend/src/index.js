c535547501a1aec37b437c9c3d1cbc1ad809a6a8
Raghav Dwivedi <rjkb143@gmail.com>
2025-05-14 19:01:21 +0530

    ADDED react_router_dom TO deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS,
     credentials: true
 }))
 
